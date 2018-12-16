a5226bf31a9455a16645da3b79f15b036ff12660
fatemamelg <felgebalius@gmail.com>
2018-12-16 13:04:33 +0200

    changing backend path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  path: 'http://localhost:3000'
-  //path: 'http://ps-social-f.herokuapp.com'
+  //path: 'http://localhost:3000'
+  path: 'http://ps-social-f.herokuapp.com'
 };
 
 /*
